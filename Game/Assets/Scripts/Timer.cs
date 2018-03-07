7274ec40a299e0f16dc705c98431610246343a30
Jay Yang <hjyang1@uci.edu>
2018-03-07 11:12:49 -0800

    timer added

---
+++ Game/Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float timeRemaining = 99;
+    public Text timerText;
+	// Use this for initialization
+	void Start () {
+        timerText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeRemaining -= Time.deltaTime;
+        timerText.text = timeRemaining.ToString("f2");
+	}
+}
